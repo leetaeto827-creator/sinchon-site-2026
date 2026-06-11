097a99447a0518064f790cc6950e0edb7b3a2e2d
leetaeto827 <leetaeto827@gmail.com>
2026-06-12 07:13:25 +0900

    google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "신촌 프리미엄 벗방 셔츠룸 | 이달인 실장",
   description: "신촌 프리미엄 벗방 셔츠룸. 전화 문의 및 상담 가능.",
+
+  verification: {
+    google: "mHVDHFzN6MYAfdGj6gkj0BMizBgM3Y5TJjh7K-WOg-g",
+  },
 };
 
 export default function RootLayout({
