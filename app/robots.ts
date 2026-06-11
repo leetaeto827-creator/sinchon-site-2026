daae21944f9f001acfb66464947e9bb3955a13b0
leetaeto827 <leetaeto827@gmail.com>
2026-06-12 07:40:07 +0900

    add robots

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://sinchon-site-2026.vercel.app/sitemap.xml",
+  };
+}
